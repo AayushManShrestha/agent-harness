aeac320c332a0da88fe0319edb38697564ac4ee9
Aayush Man Shrestha <aayush.m.shrestha@gmail.com>
2026-09-21 12:55:41 +0545

    feat: add async OpenRouter LLM client with streaming support

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main() -> None:
-    print("Hello from agent-harness!")
+from client.llm_client import LLMClient
+import asyncio
+
+async def main() -> None:
+    client = LLMClient()
+    messages = [{
+        'role': 'user',
+        'content': "Hello"
+    }]
+    async for event in client.chat_completion(messages, True):
+        print(event)
+    print("done")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
